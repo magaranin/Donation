2f0a51135823dc65c4d74b180f3fccc3af42e2b2
Margarita Sirbu <margaritapetru@hotmail.com>
2022-04-09 22:37:38 -0700

    Add index.html page

---
+++ donation/static/donation/script.js
@@ -0,0 +1,23 @@
+const imgs = document.getElementById('imgs');
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+    idx++;
+    changeImage();
+}
+
+function changeImage() {
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+    else if (idx < 0) {
+        idx = img.length - 1;
+    }
+    imgs.style.transform = `translateX(${-idx * 900}px)`
+}
+
+
