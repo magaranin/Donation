94f74a50b3689c512308590e1320bba00669865b
magaranin <margaritapetru@hotmail.com>
2022-10-08 18:00:38 -0700

    Add missing changes

--- donation/models.py
+++ donation/models.py
@@ -26,26 +26,30 @@ class Gender(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-
-class WhoPays(models.Model):
-    name = models.CharField(max_length = 200)
-    class Meta: 
-        verbose_name_plural = "Who Pays"  
-    def __str__(self):
-        return f"{self.name}"
-
 class ListingOffer(models.Model):
+    #statuses active, claimed, removed, pending.
+    STATUS_CHOICES = [
+        ("active", "active"),
+        ("claimed", "claimed"),
+        ("pending", "pending"),
+        ("removed", "removed"),
+    ]
+    WHO_PAYS_CHOICES = [
+        ("Donor", "Willing to pay for delivery"),
+        ("Other", "Need a sponsor"),
+    ]
     title = models.CharField(max_length=75)
     description = models.CharField(null=True, max_length=1000)
     post_date = models.DateTimeField(auto_now=True)
     images = models.FileField(upload_to='images', blank=True, default="images/listing_default.jpg")
     categories = models.ManyToManyField(Category)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="owner")
-    who_pays = models.ForeignKey(WhoPays, on_delete=models.CASCADE)
+    who_pays = models.CharField(max_length=30, choices=WHO_PAYS_CHOICES, default="Paying")
     shipping_cost = models.DecimalField(max_digits=5, decimal_places=2, default="0")
     gender = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True)
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="recipient")
     claimed_time = models.DateTimeField(null=True)
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default="active")
     def __str__(self):
         return f"{self.title}"
 
@@ -63,3 +67,6 @@ class Country(models.Model):
         verbose_name_plural = "Countries"  
     def __str__(self):
         return f"{self.name}"
+
+class Payment(models.Model):
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
