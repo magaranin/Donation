6fcae6a3f5fc9d6c4ea6c343512d0f104fcd0d7c
Margarita Sirbu <margaritapetru@hotmail.com>
2024-01-03 22:38:35 -0800

    Change timezone from UTC to PST

--- donation/models.py
+++ donation/models.py
@@ -40,7 +40,7 @@ class ListingOffer(models.Model):
     ]
     title = models.CharField(max_length=75)
     description = models.CharField(null=True, max_length=1000)
-    post_date = models.DateTimeField(auto_now=True)
+    post_date = models.DateTimeField(auto_now_add=True)
     images = models.FileField(upload_to='images', blank=True, default="images/listing_default.jpg")
     categories = models.ManyToManyField(Category)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="owner")
